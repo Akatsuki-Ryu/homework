5f278ead84c9b1d03356a43f1390defefec44a6a
Akabox <Akabox>
2020-09-30 11:25:14 +0300

    clear up copied files

--- homework/server/contentscheck.py
+++
@@ -1,8 +0,0 @@
-class contentscheckclass(object):
-    def __init__(self):
-        print("init of contents check class")
-        return
-
-    def contentscheck(content):
-        print("contents check func == " + content)
-        return "contents check call back"
